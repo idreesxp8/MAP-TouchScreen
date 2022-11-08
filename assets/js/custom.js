d4b5d60cb4eebc8434a725e56b7978534e925fad
flopmart <34133163+asadnisar98@users.noreply.github.com>
2022-11-08 12:28:12 +0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $('.slider-box').fadeOut()
+  $('.main_hotspots .hotspot').click(function () {
 
+    
+    var element = $(this).closest('.hotspot').attr('id')
 
-    $('.slider-box').fadeOut();
-    $('.main_hotspots .hotspot').click(function() {
-        var element = $(this).closest('.hotspot').attr('id');
-        $('.hp_numbers h2').removeClass('active');
-        $(this).find('.hp_numbers h2').addClass('active');
+    var el1 = $('.' + element).hasClass('active');
+
+    if(!el1) {
         $('.slider-box').fadeOut();
-        $('.'+element).fadeIn('slow');
-        $('.slick-slider').slick('refresh');
-        });
+        $('.slider-box').removeClass('active')
+        $('.hp_numbers h2').removeClass('active')
+        $(this).find('.hp_numbers h2').addClass('active')
+       
+        $('.' + element).fadeIn('slow')
+        $('.' + element).addClass('active')
+        $('.slick-slider').slick('refresh')
+    }
 
 
-     $('.close_btn').click(function() {
-        console.log('close');
 
-        $(this).closest('.slider-box').fadeOut('slow');
-        $('.hp_numbers h2').removeClass('active');
-        $('#slick-slider').slick('refresh');
-     });
+  })
 
-  });   
+  $('.close_btn').click(function () {
+    var el = $(this).closest('.slider-box');
+    var thus = $(this)
+
+    if(el.hasClass('active')){
+        thus.closest('.slider-box').fadeOut('slow')
+        $('.hp_numbers h2').removeClass('active')
+        $('.slider-box').removeClass('active')
+        $('#slick-slider').slick('refresh')
+    }
+
+
+  })
+})
